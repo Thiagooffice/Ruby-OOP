9284fc41ca72c5d948f76aff686c698e1fe341bc
Thiago <thiagooffice2@gmail.com>
2023-08-05 20:47:12 -0300

    feat: Inheritance and Polymorphism

---
+++ animal.rb
@@ -0,0 +1,44 @@
+class Animal
+    attr_reader :nome
+
+    def initialize(nome)
+      @nome = nome
+    end
+
+    def comunicar
+        puts "Olá, eu sou a classe mãe, ou super classe."
+    end
+
+end
+
+class Cachorro < Animal
+    attr_reader :raça
+
+    def initialize(nome, raça)
+        super(nome)
+        @raça = raça
+    end
+
+    def comunicar
+        puts "Au, au, au! = Eu sou a classe filha!"
+    end
+end
+
+class Gato < Animal
+    
+    def initialize(nome, raça)
+        super(nome)
+        @raça = raça
+    end
+end
+
+bob = Cachorro.new("Bob", "Poodle")
+
+puts bob.nome
+puts bob.raça
+bob.comunicar
+puts "------"
+gato = Gato.new("Xaninho", "Siamês")
+puts gato.nome
+puts gato.raça
+gato.comunicar
