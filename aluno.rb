6058b50e02450b98dc98e5ba4fbfa4e126624ce9
Thiago <thiagooffice2@gmail.com>
2023-08-05 20:19:30 -0300

    feat: classes and objects

--- aluno.rb
+++ aluno.rb
@@ -7,11 +7,16 @@ class Aluno
     end
 
     def mostrar_dados
-        puts "Nome: #{@nome}, Idade: #{@idade}"
+        puts "Nome: #{@nome} #{@last_name}, Idade: #{@idade}"
+    end
+
+    def add_last_name(last_name)
+        @last_name = last_name
     end
 end
 
 a1 = Aluno.new("Thiago", 35)
+a1.add_last_name("Leal de souza")
 
 # a1.nome = "Thiago"
 # a1.idade = 24
